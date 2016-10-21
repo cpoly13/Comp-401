8c3f4bb36b181ee943eed3c79362a71adbbe9bd9
seph1 <seph1@DESKTOP-TTD9H2E>
2016-10-20 23:46:37 -0400

    Not Sure....

---
+++ src/a5/cpoly13/A6JediTest.java
@@ -0,0 +1,59 @@
+package a5.cpoly13;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+
+import a6jedi.*;
+
+/*
+ * Tests zigzag method of A6Jedi Iterator
+ * @author Chris Polychronides
+ */
+public class A6JediTest {
+
+	static public String[] getTestNames() {
+		String[] test_names = new String[2];
+
+		test_names[0] = "exampleTest";
+		test_names[1] = "zigzagIteratorTester";
+
+		return test_names;
+	}
+
+	@Test
+	public void exampleTest() {
+	}
+
+	/*
+	 * Tests zigzag method of A6Jedi Iterator
+	 */
+	@Test
+	public void zigzagIteratorTester() {
+		Picture testPic = new PictureImpl(3, 3);
+		Iterator<Pixel> zigzags = testPic.zigzag();
+
+		for (int i = 0; i < testPic.getWidth(); i++) {
+
+			for (int n = 0; n < testPic.getHeight(); n++) {
+				Pixel random = new GrayPixel(Math.random());
+				Coordinate xy = new Coordinate(i, n);
+				testPic.setPixel(xy, random);
+			}
+		}
+
+		assertEquals("Zigzag is zigzaging wrong", zigzags.next().getChar(), testPic.getPixel(0, 0).getChar());
+
+		assertEquals("Zigzag is zigzaging wrong", zigzags.next().getChar(), testPic.getPixel(1, 0).getChar());
+		assertEquals("Zigzag is zigzaging wrong", zigzags.next().getChar(), testPic.getPixel(0, 1).getChar());
+		assertEquals("Zigzag is zigzaging wrong", zigzags.next().getChar(), testPic.getPixel(0, 2).getChar());
+		assertEquals("Zigzag is zigzaging wrong", zigzags.next().getChar(), testPic.getPixel(1, 1).getChar());
+		assertEquals("Zigzag is zigzaging wrong", zigzags.next().getChar(), testPic.getPixel(2, 0).getChar());
+		assertEquals("Zigzag is zigzaging wrong", zigzags.next().getChar(), testPic.getPixel(2, 1).getChar());
+		assertEquals("Zigzag is zigzaging wrong", zigzags.next().getChar(), testPic.getPixel(1, 2).getChar());
+		assertEquals("Zigzag is zigzaging wrong", zigzags.next().getChar(), testPic.getPixel(2, 2).getChar());
+
+	}
+}
