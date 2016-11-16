3063650e6846524dea8b298119e888a4764d5264
Chris Polychronides <cpoly13@gmail.com>
2016-11-16 15:11:46 -0500

    PixelInstpector (alpha) added

---
+++ Assignment8/src/a8/PixelInspector.java
@@ -0,0 +1,28 @@
+package a8;
+import java.awt.BorderLayout;
+import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class PixelInspector {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Picture p=A8Helper.readFromURL("http://www.cs.unc.edu/~kmp/kmp.jpg");
+		SimplePictureViewWidget temp =new SimplePictureViewWidget(p,"Hope this works...");
+		
+		JFrame mainFrame=new JFrame();
+		mainFrame.setTitle("Assignment 8 Pixel Inspector");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel topPanel= new JPanel();
+		topPanel.setLayout(new BorderLayout());
+		topPanel.add(temp, BorderLayout.CENTER);
+		
+		mainFrame.setContentPane(topPanel);
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+	}
+
+}
