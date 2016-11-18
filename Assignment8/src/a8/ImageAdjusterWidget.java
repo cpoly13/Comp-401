675a1dc4fce636ccb3cf60f36b07d08d05bbf554
Chris Polychronides <cpoly13@gmail.com>
2016-11-18 14:24:35 -0500

    Blur starting to operate

--- Assignment8/src/a8/ImageAdjusterWidget.java
+++ Assignment8/src/a8/ImageAdjusterWidget.java
@@ -9,9 +9,14 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class ImageAdjusterWidget extends JPanel {
+public class ImageAdjusterWidget extends JPanel implements ChangeListener {
+	
 	private PictureView picture_view;
+	private final Picture originalPic;
+	private Picture editedPic;
 	private JPanel panel;
 	private JSlider blurSlider;
 	private JLabel blur;
@@ -20,6 +25,9 @@ public class ImageAdjusterWidget extends JPanel {
 		setLayout(new BorderLayout());
 		
 		picture_view=new PictureView(picture.createObservable());
+		originalPic=picture;
+		editedPic= new PictureImpl(picture.getWidth(),picture.getHeight());
+		
 		add(picture_view, BorderLayout.CENTER);
 		
 		panel=new JPanel();
@@ -30,10 +38,32 @@ public class ImageAdjusterWidget extends JPanel {
 		blurSlider.setMajorTickSpacing(1);
 		blurSlider.setLabelTable(blurSlider.createStandardLabels(1));
 		blurSlider.setPaintLabels(true);
+		blurSlider.addChangeListener(this);
+		
 		panel.setLayout(new BoxLayout(panel,0));
 		panel.add(blur);
 		panel.add(blurSlider);
 		add(panel,BorderLayout.SOUTH);
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		// TODO Auto-generated method stub
+		int blurValue=blurSlider.getValue();
+		if (blurValue>0){
+			blurPicture(blurValue);
+			picture_view.setPicture(editedPic.createObservable());
+		}
+		
+	}
+	
+	public void blurPicture(int value){
+		Pixel ran=new ColorPixel(0.5,0.5,0.5);
+		for (int i=0; i<editedPic.getWidth();i++){
+			for(int n=0; n<editedPic.getHeight(); n++){
+				editedPic.setPixel(i, n,ran );
+			}
+		}
 	}
 	
 	
