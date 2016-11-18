00ca175345fbaa8644fe07a40a0a682a8b8c692e
Chris Polychronides <cpoly13@gmail.com>
2016-11-18 16:25:20 -0500

    Blur Slider mostly working at value 1. Missing sides and bottom

--- Assignment8/src/a8/ImageAdjusterWidget.java
+++ Assignment8/src/a8/ImageAdjusterWidget.java
@@ -50,18 +50,65 @@ public class ImageAdjusterWidget extends JPanel implements ChangeListener {
 	public void stateChanged(ChangeEvent e) {
 		// TODO Auto-generated method stub
 		int blurValue=blurSlider.getValue();
-		if (blurValue>0){
+		if (blurValue!=0){
 			blurPicture(blurValue);
 			picture_view.setPicture(editedPic.createObservable());
+		}
+		
+		else{
+			picture_view.setPicture(originalPic.createObservable());
 		}
 		
 	}
 	
 	public void blurPicture(int value){
-		Pixel ran=new ColorPixel(0.5,0.5,0.5);
+		
 		for (int i=0; i<editedPic.getWidth();i++){
 			for(int n=0; n<editedPic.getHeight(); n++){
-				editedPic.setPixel(i, n,ran );
+				if (i==0 && n==0){
+					double averageRed=(originalPic.getPixel(i+1,n).getRed()+originalPic.getPixel(i, n+1).getRed())/2.0;
+					double averageGreen=(originalPic.getPixel(i+1,n).getGreen()+originalPic.getPixel(i, n+1).getGreen())/2.0;
+					double averageBlue=(originalPic.getPixel(i+1,n).getBlue()+originalPic.getPixel(i, n+1).getBlue())/2.0;
+					
+					editedPic.setPixel(i, n, new ColorPixel(averageRed,averageGreen,averageBlue));
+				}
+				else if(i==0 && n>0 && n+1<editedPic.getHeight()){
+					double averageRed=(originalPic.getPixel(n-1, n).getRed()+ originalPic.getPixel(i+1,n).getRed()+originalPic.getPixel(i, n+1).getRed())/3.0;
+					double averageGreen=(originalPic.getPixel(n-1, n).getGreen()+originalPic.getPixel(i+1,n).getGreen()+originalPic.getPixel(i, n+1).getGreen())/3.0;
+					double averageBlue=(originalPic.getPixel(n-1, n).getBlue() + originalPic.getPixel(i+1,n).getBlue()+originalPic.getPixel(i, n+1).getBlue())/3.0;
+					
+					editedPic.setPixel(i, n, new ColorPixel(averageRed,averageGreen,averageBlue));
+				}
+				
+				else if(i==0 && n+1==editedPic.getHeight()){
+					double averageRed=(originalPic.getPixel(n-1, n).getRed()+ originalPic.getPixel(i+1,n).getRed())/2.0;
+					double averageGreen=(originalPic.getPixel(n-1, n).getGreen()+originalPic.getPixel(i+1,n).getGreen())/2.0;
+					double averageBlue=(originalPic.getPixel(n-1, n).getBlue() + originalPic.getPixel(i+1,n).getBlue())/2.0;
+					
+					editedPic.setPixel(i, n, new ColorPixel(averageRed,averageGreen,averageBlue));
+					
+				}
+				
+				else if(i>0 && n==0 && i+1<editedPic.getWidth()){
+					double averageRed=(originalPic.getPixel(i-1, n).getRed()+ originalPic.getPixel(i+1,n).getRed()+originalPic.getPixel(i, n+1).getRed())/3.0;
+					double averageGreen=(originalPic.getPixel(i-1, n).getGreen()+originalPic.getPixel(i+1,n).getGreen()+originalPic.getPixel(i, n+1).getGreen())/3.0;
+					double averageBlue=(originalPic.getPixel(i-1, n).getBlue() + originalPic.getPixel(i+1,n).getBlue()+originalPic.getPixel(i, n+1).getBlue())/3.0;
+					
+					editedPic.setPixel(i, n, new ColorPixel(averageRed,averageGreen,averageBlue));
+					
+					
+				}
+				
+				else if(i>0 && n>0 && i+1<editedPic.getWidth()&& n+1<editedPic.getHeight()){
+					double averageRed=(originalPic.getPixel(i, n-1).getRed()+ originalPic.getPixel(i-1, n).getRed()+ originalPic.getPixel(i+1,n).getRed()+originalPic.getPixel(i, n+1).getRed())/4.0;
+					double averageGreen=(originalPic.getPixel(i, n-1).getGreen()+originalPic.getPixel(i-1, n).getGreen()+originalPic.getPixel(i+1,n).getGreen()+originalPic.getPixel(i, n+1).getGreen())/4.0;
+					double averageBlue=(originalPic.getPixel(i, n-1).getBlue()+originalPic.getPixel(i-1, n).getBlue() + originalPic.getPixel(i+1,n).getBlue()+originalPic.getPixel(i, n+1).getBlue())/4.0;
+					
+					editedPic.setPixel(i, n, new ColorPixel(averageRed,averageGreen,averageBlue));
+					
+				}
+				
+				
 			}
 		}
 	}
